a396df60dd6af7b4c1a64ab5126eee19c5fedab1
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-18 23:46:35 +0300

    Createfirst controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_225722) do
+ActiveRecord::Schema.define(version: 2021_11_05_201425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
+    t.integer "position"
+    t.boolean "visible"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", limit: 25
