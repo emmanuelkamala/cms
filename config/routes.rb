381b80b8a8e1963d16b460b3eac903cdfb22f9d8
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-11-19 21:21:47 +0300

    Create a controller for subjects and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'subjects/index'
+  get 'subjects/show'
+  get 'subjects/new'
+  get 'subjects/edit'
+  get 'subjects/delete'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
